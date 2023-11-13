b7f2fd5cf34fedbd24684c7a9293394d04502302
smznahid <sheikh.nahid0802@gmail.com>
2023-11-13 09:51:47 +0000

    implements yaml_loader

--- db_utils.py
+++ db_utils.py
@@ -1,3 +1,14 @@
+import yaml
+
+
 class RDSDatabaseConnector:
+
     def __init__(self, credentials):
         pass
+
+    @staticmethod
+    def yaml_loader():
+        with open('credentials.yaml', 'r') as credentials:
+            credentials_dict = yaml.safe_load(credentials)
+            return credentials_dict
+    
