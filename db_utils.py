afe29e686d496d76bcbd541f382b391773b5d11b
smznahid <sheikh.nahid0802@gmail.com>
2023-11-13 09:54:05 +0000

    implements __init__ properly to RDSDatabaseConnecter

--- db_utils.py
+++ db_utils.py
@@ -4,7 +4,13 @@ import yaml
 class RDSDatabaseConnector:
 
     def __init__(self, credentials):
-        pass
+        self.DATABASE_TYPE = 'postgresql'
+        self.DBAPI = 'psycopg2'
+        self.HOST = credentials["RDS_HOST"]
+        self.USER = credentials["RDS_USER"]
+        self.PASSWORD = credentials["RDS_PASSWORD"]
+        self.DATABASE = credentials["RDS_DATABASE"]
+        self.PORT = credentials["RDS_PORT"]
 
     @staticmethod
     def yaml_loader():
