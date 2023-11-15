4b8fb1a8e88c5ff963ef3f9d78182b3cff6711f6
smznahid <sheikh.nahid0802@gmail.com>
2023-11-15 09:57:36 +0000

    finish  implementing DataTransform Class

--- transform_data.py
+++ transform_data.py
@@ -3,9 +3,28 @@ import pandas as pd
 
 class DataTransform:
 
+    '''
+    class to transform data in a pandas DataFrame
+    '''
 
     @staticmethod
     def convert_columns(df):
-        # TODO: convert more columns, remove print statement.
-        df.grade = df.grade.astype('category')
+        '''
+        a static method to convert different dtypes in my DataFrame.
+
+        Args:
+            df (pd.DataFrame): the DataFrame of which we want to convert the columns from
+        Returns:
+            None
+        '''
+
+        # grade, subgrade, home_ownership, loan_status, purpose, application_type, verification_status -> category
+        df[['grade','sub_grade','home_ownership','loan_status','purpose','application_type','verification_status']] = df[['grade','sub_grade','home_ownership','loan_status','purpose','application_type','verification_status']].astype('category')
+        # issue_date, earliest_credit_line, last_payment_date, next_payment_date, last_credit_pull_date -> datetime64[ns]
+        datetime_list = ['issue_date', 'earliest_credit_line', 'last_payment_date', 'next_payment_date', 'last_credit_pull_date']
+        for column in datetime_list:
+            df[column] = pd.to_datetime(df[column], format='%b-%Y', errors='ignore')
+        
+        # payment_plan -> bool
+        df.payment_plan = df.payment_plan.astype('bool')
         print(df.dtypes)
