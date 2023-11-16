ce6de23af85b6966f69cc8a87818db3f0d8aa507
smznahid <sheikh.nahid0802@gmail.com>
2023-11-16 08:53:58 +0000

    adds more methods

--- get_information.py
+++ get_information.py
@@ -5,9 +5,21 @@ class DataFrameInfo:
 
 
     @staticmethod
-    def describe_df(df):
-        # TODO: complete describe_df
+    def datatypes(df):
+        print(df.dtypes)
+
+    @staticmethod
+    def statistics(df):
+        # return transposed .describe() as large number of columns.
+        return df.describe().T
+    
+    @staticmethod
+    def distinct_values(df):
         pass
+
+    @staticmethod
+    def df_shape(df):
+        return df.shape()
 
     @staticmethod
     def percent_null(df):
