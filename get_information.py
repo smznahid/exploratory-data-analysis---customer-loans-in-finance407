21684fae0a30a12d8847e8f79bc944ebd3da5e4d
smznahid <sheikh.nahid0802@gmail.com>
2023-11-16 08:40:32 +0000

    init describe_df

--- get_information.py
+++ get_information.py
@@ -2,7 +2,13 @@ import pandas as pd
 
 
 class DataFrameInfo:
-    
+
+
+    @staticmethod
+    def describe_df(df):
+        # TODO: complete describe_df
+        pass
+
     @staticmethod
     def percent_null(df):
         # TODO: percent_null
