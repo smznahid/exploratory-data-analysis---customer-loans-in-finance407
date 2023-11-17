1d7a8f165f3b2fdc6c9296e8d1644cd24dd55526
smznahid <sheikh.nahid0802@gmail.com>
2023-11-17 13:06:23 +0000

    implement drop_column method

--- transform_data.py
+++ transform_data.py
@@ -23,7 +23,16 @@ class DataTransform:
         # issue_date, earliest_credit_line, last_payment_date, next_payment_date, last_credit_pull_date -> datetime64[ns]
         datetime_list = ['issue_date', 'earliest_credit_line', 'last_payment_date', 'next_payment_date', 'last_credit_pull_date']
         for column in datetime_list:
-            df[column] = pd.to_datetime(df[column], format='%b-%Y', errors='ignore')
+            df[column] = pd.to_datetime(df[column], format='%b-%Y')
         
         # payment_plan -> bool
         df.payment_plan = df.payment_plan.astype('bool')
+    
+
+    @staticmethod
+    def drop_columns(df):
+        #drop mths_since_last_delinq, mths_since_last_record, next_payment_date and mths_since_last_major_derog (TOO MANY NULLs)
+       df.drop(columns=['mths_since_last_delinq', 'mths_since_last_record', 'next_payment_date', 'mths_since_last_major_derog'], inplace=True)
+
+       #drop Unnamed: 0 and policy_code (static value thus not important to insights)
+       df.drop(columns=['Unnamed: 0', 'policy_code'], inplace=True)
